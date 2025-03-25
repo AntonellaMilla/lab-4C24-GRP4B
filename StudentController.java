894d82690f825a8efd0a786b9d656f6fa7ad76aa
Anjeli Verastigue <anjeli.verastigue@tecsup.edu.pe>
2025-03-25 17:51:18 -0500

    Initial commit with Maven project and StudentController.java

---
+++ StudentController.java
@@ -0,0 +1,7 @@
+package pe.edu.tecsup.lab02;
+
+public class StudentController {
+    public static void main(String[] args) {
+        System.out.println("Hello from StudentController!");
+    }
+}
