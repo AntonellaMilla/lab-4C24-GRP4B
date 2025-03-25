adbf215bf27a8cdb4769cdbef6780c56b97f363b
AriadnaNolasco <ariadna.nolasco@tecsup.edu.pe>
2025-03-25 18:50:05 -0500

    Added StudentView

---
+++ src/main/java/pe/edu/tecsup/lab02/StudentView.java
@@ -0,0 +1,7 @@
+package pe.edu.tecsup.lab02;
+
+public class StudentView {
+    public void displayStudent(String name, int age) {
+        System.out.println("Student Name: " + name + ", Age: " + age);
+    }
+}
